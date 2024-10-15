146c44161f7e85cbec8a05d58d88a60cd1938cf3
CBrazell23 <colebrazell@gmail.com>
2024-10-15 08:52:04 -0700

    Part 1 done. ugly

---
+++ assn2/test.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+randomRows1 = np.random.choice(range(10), 3, replace=False)
+randomRows2 = np.random.choice(range(10), 3, replace=False)
+randomRows3 = np.random.choice(range(10), 3, replace=False)
+
+print(randomRows1)
+print(randomRows2)
+print(randomRows3)
